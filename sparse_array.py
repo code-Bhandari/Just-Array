9faaf0afcf40b1b0c707a4640db8177f4d8d834d
Shishant-Bhandari <90854755+code-Bhandari@users.noreply.github.com>
2023-03-06 09:16:28 +0530

    Create sparse_array.py

---
+++ sparse_array.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+def matchingStrings(strings, queries):
+    
+    s = Counter(strings)
+    result = []
+    
+    
+    for q in queries:
+        result.append(s[q])
+    
+    return result
